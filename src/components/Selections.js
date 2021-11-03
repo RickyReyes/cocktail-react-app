abf97590823d9f0336048b727a64d8f80b366fb3
Ricky Reyes <luisereyes52@gmail.com>
2021-11-03 12:23:03 -0700

    rendering correct colors in Selections

--- src/components/Selections.js
+++ src/components/Selections.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from 'react-dom';
 
 class Selections extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return <h2>yooooo</h2>
-    }
+	constructor(props) {
+		super(props);
+		this.removeItem = this.removeItem.bind(this);
+	}
+
+	// NOT FUNCTIONAL YET
+	// for removing items by clicking in Selections component
+	removeItem() {
+		return;
+	}
+	render() {
+		return (
+			<ul className="selections">
+				{this.props.mainList.map((item, idx) => (
+					<li key={idx} className="selection" onClick={this.removeItem}
+					style={{backgroundColor: item[1]}}>
+						{item[0]}
+					</li>
+					
+				))}
+			</ul>
+		);
+	}
 }
 
-export default Selections
+export default Selections;
